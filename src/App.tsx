3e6ddffb06b786a8fa8500bd93a748545228b7df
Paul Pham <148553+cryptogoth@users.noreply.github.com>
2023-03-31 20:57:31 -0400

    Add Jitsi react component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { JitsiMeeting } from '@jitsi/react-sdk';
 import reactLogo from './assets/react.svg'
 import './App.css'
 
@@ -8,18 +9,28 @@ function App() {
   return (
     <div className="App">
       <h1>Code With Friends</h1>
-      <div className="card">
-        <p>
-          A learning collective<br/>
-          for coders of all levels<br/>
-          to hone their skills,<br/>
-          make friends,<br/>
-          and have fun.
-        </p>
+
+      <div id="main">
+
+       <div id="jitsi-container">
+        <JitsiMeeting
+          roomName = { 'Rooom with a View' } // make sure it's a good one!
+        />
+       </div>
+
+        <div className="card">
+          <p>
+            A learning collective<br/>
+            for coders of all levels<br/>
+            to hone their skills,<br/>
+            make friends,<br/>
+            and have fun.
+          </p>
+        </div>
+        <a href="https://www.meetup.com/detroit-code-with-friends-meetup/">
+          Visit our Meetup group to see our upcoming events. 
+        </a>
       </div>
-      <a href="https://www.meetup.com/detroit-code-with-friends-meetup/">
-        Visit our Meetup group to see our upcoming events. 
-      </a>
     </div>
   )
 }
